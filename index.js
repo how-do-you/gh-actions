6c4f3b6d8c270a4031c4b3e4790eae0e0291a6db
Simon Hyll <simon@adaptivehedge.com>
2022-06-08 15:10:06 +0200

    minor fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cargo = require('./src/cargo');
 try {
     const script_to_run = core.getInput('script');
     if (script_to_run === "cargo") {
-        cargo();
+        cargo.run();
     }
 } catch (error) {
     core.setFailed(error.message);
--- src/cargo.js
+++ src/cargo.js
@@ -1,5 +1,5 @@
 const core = require("@actions/core");
 
-export default () => {
+exports.run = () => {
     core.setOutput("version", "0.0.0");
 }
