8c3fd6ee4e8a66b403d6fbbeaf6e5ef94abda5a2
Simon Hyll <simon@adaptivehedge.com>
2022-06-08 15:14:35 +0200

    update

--- src/cargo.js
+++ src/cargo.js
@@ -1,5 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
 const core = require("@actions/core");
 
-exports.run = () => {
+
+async function* walk(dir) {
+    for await (const d of await fs.promises.opendir(dir)) {
+        const entry = path.join(dir, d.name);
+        if (d.isDirectory()) yield* walk(entry);
+        else if (d.isFile()) yield entry;
+    }
+}
+
+exports.run = async () => {
     core.setOutput("version", "0.0.0");
+    for await (const p of walk('.')) {
+        console.log(p)
+    }
 }
